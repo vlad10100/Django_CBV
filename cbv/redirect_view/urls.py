244c4ed20b5a1e6fbdc0be2f127faf6fbdfa1a16
vlad10100 <vbucaralde@gmail.com>
2022-02-06 22:57:47 +0800

    cbv redirect view

--- cbv/redirect_view/urls.py
+++ cbv/redirect_view/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from django.views.generic import RedirectView
 
-from .views import RedirectToThisPost
+from .views import RedirectToThisPost, PostPreLoadTaskView
 
 app_name = 'redirect_view'
 
 urlpatterns = [
     path('redirect/', RedirectView.as_view(url='https://www.instagram.com/'), name='instagram'),
-    path('page/<int:pk>/', RedirectToThisPost.as_view(), name='redirect_page')
+    path('page/<int:pk>/', RedirectToThisPost.as_view(), name='redirect_page'),
+    path('preload/<int:pk>/', PostPreLoadTaskView.as_view(), name='preload'),
 ]
--- cbv/redirect_view/views.py
+++ cbv/redirect_view/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
+from django.db.models import F
+
 
 from django.views.generic import RedirectView, TemplateView
 
@@ -12,6 +14,7 @@ class RedirectToThisPost(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        print(kwargs)
         context["posts"] = get_object_or_404(Post, pk=self.kwargs.get('pk'))
         return context 
 
@@ -19,9 +22,9 @@ class PostPreLoadTaskView(RedirectView):
     pattern_name = 'redirect_view:redirect_page'
 
     def get_redirect_url(self, *args, **kwargs):
-        post = Post.objects.get(pk=kwargs('pk'))
+        post = Post.objects.filter(pk=kwargs['pk'])
         post.update(count=F('count')+1)
+        print(self.count)
 
         return super().get_redirect_url(*args, **kwargs)
 
-        
