dbb30d9badfab9232944aef4e6a28711ebdbfdc1
vlad10100 <vbucaralde@gmail.com>
2022-02-06 22:41:26 +0800

    cbv

--- cbv/template_view/models.py
+++ cbv/template_view/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=20)
     post = models.TextField()
+    count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
--- cbv/template_view/views.py
+++ cbv/template_view/views.py
@@ -7,7 +7,7 @@ from .models import Post
 
 
 class PostTemplateView(TemplateView):
-    template_name = 'cbv/page_one.html'
+    template_name = 'template_view/page_one.html'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
