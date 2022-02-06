3851a450fe123092169c59158ca3b74d6acfdb96
vlad10100 <vbucaralde@gmail.com>
2022-02-07 04:19:23 +0800

    cbv permissions

--- cbv/crud/views.py
+++ cbv/crud/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
+
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
 from django.views.generic import CreateView, UpdateView, DeleteView
 
@@ -18,8 +20,29 @@ class UpdateBook(UpdateView):
     form_class = BookForm 
     success_url = reverse_lazy('detail_view:book_list')
 
-class DeleteView(DeleteView): # without confirmation
+
+
+
+# User access Mixin!!
+class UserAccessMixin(PermissionRequiredMixin):
+    def dispatch(self, request, *args, **kwargs):
+        if not self.has_permission():
+            return redirect('/detail/books/')
+
+
+        return super(UserAccessMixin, self).dispatch(request, *args, **kwargs)
+    
+
+
+
+
+class DeleteView(UserAccessMixin, DeleteView): # without confirmation
+    raise_exception = False
+    permission_required = 'detail_view.delete_book'       # if there is no permission to delete, user will be redirected
+
     model = Book
     context_object_name = 'book'
     success_url = reverse_lazy('detail_view:book_list')
 
+    def get_redirect_field_name(self):
+        return reverse_lazy('detail_view:book_list')
