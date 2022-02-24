80854fa000d52278c0cc20c6c93f7216b583905a
vlad10100 <vbucaralde@gmail.com>
2022-02-24 19:16:41 +0800

    todo-drf-react

---
+++ todo_drf_react/todo/views.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets
+
+from todo.models import ToDo
+from todo.serializers import ToDoSerializer
+
+
+class ToDoViewSet(viewsets.ModelViewSet):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer 
+
+
+    
