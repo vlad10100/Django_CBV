ea2a6d62bfa8624a565af28ca29ea282cc8180b6
vlad10100 <vbucaralde@gmail.com>
2022-02-24 19:15:47 +0800

    todo-drf-react

--- todo_drf_react/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from todo.urls import router
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls)),
-]
--- todo_drf_react/todo/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path 
-from rest_framework import routers 
-
-from todo.views import ToDoViewSet 
-
-router = routers.DefaultRouter()
-router.register('todo', ToDoViewSet)
